5da9db819ee3df2b9307a4c2c17ce861ab34a5ef
SantyM19 <santiagomejiad19@hotmail.com>
2020-08-21 18:58:57 -0500

    Adding all Keys Function

--- js/ReadKeys.js
+++ js/ReadKeys.js
@@ -10,6 +10,63 @@ function ReadKeys() {
     document.getElementById("7").addEventListener("click", displayDate7);
     document.getElementById("8").addEventListener("click", displayDate8);
     document.getElementById("9").addEventListener("click", displayDate9);
+    document.getElementById("on").addEventListener("click", on);
+    document.getElementById("sign").addEventListener("click", sign);
+    document.getElementById("dividido").addEventListener("click", divide);
+    document.getElementById("raiz").addEventListener("click", raiz);
+    document.getElementById("por").addEventListener("click", por);
+    document.getElementById("menos").addEventListener("click", rest);
+    document.getElementById("punto").addEventListener("click", punto);
+    document.getElementById("igual").addEventListener("click", igual);
+    document.getElementById("mas").addEventListener("click", mas);
+
+    function on() {
+        document.getElementById("display").innerHTML = "0";
+    }
+
+    function sign() {
+        console.log("----");
+    }
+
+    function divide() {
+        var d = document.getElementById("display");
+        num = "/";
+        document.getElementById("display").innerHTML = d.innerHTML + num;
+    }
+
+    function raiz() {
+        console.log("raiz");
+    }
+
+    function por() {
+        var d = document.getElementById("display");
+        num = "*";
+        document.getElementById("display").innerHTML = d.innerHTML + num;
+    }
+
+    function rest() {
+        var d = document.getElementById("display");
+        num = "-";
+        document.getElementById("display").innerHTML = d.innerHTML + num;
+    }
+
+    function punto() {
+        var d = document.getElementById("display");
+        num = ".";
+        document.getElementById("display").innerHTML = d.innerHTML + num;
+    }
+
+    function igual() {
+        console.log("igual");
+    }
+
+    function mas() {
+        var d = document.getElementById("display");
+        num = "+";
+        document.getElementById("display").innerHTML = d.innerHTML + num;
+    }
+
+    //Function Numbers
 
     function displayDate0() {
         var d = document.getElementById("display");
